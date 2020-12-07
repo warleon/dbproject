e114a3a247ce84268764445705b29c413fbd906c
Sebastian Knell <sebastianknell@hotmail.com>
2020-12-06 20:36:08 -0500

    fixed most constraints

--- constraints.sql
+++ constraints.sql
@@ -1,44 +1,48 @@
+--usuario constraints
+ALTER TABLE usuario
+ADD CONSTRAINT usuario_pk_nombre
+PRIMARY KEY (Nombre);
+
+
 --servidor constraints
 ALTER TABLE servidor
-ADD CONSTRAINT pk_nombre
+ADD CONSTRAINT serv_pk_nombre
 PRIMARY KEY (Nombre);
 
 ALTER TABLE servidor
-ADD CONSTRAINT fk_unombre
+ADD CONSTRAINT serv_fk_unombre
 FOREIGN KEY (UsuarioNombre)
 REFERENCES usuario(Nombre);
 
---usuario constraints
-ALTER TABLE usuario
-ADD CONSTRAINT pk_nombre
-PRIMARY KEY (Nombre);
 
 --sala constraints
 ALTER TABLE sala
-ADD CONSTRAINT pk_codigo
+ADD CONSTRAINT sala_pk_codigo
 PRIMARY KEY (codigo);
 
 ALTER TABLE sala
-ADD CONSTRAINT fk_snombre
+ADD CONSTRAINT sala_fk_snombre
 FOREIGN KEY (ServidorNombre)
 REFERENCES servidor(Nombre);
 
-ALTER TABLE sala
+// TODO fix
+/* ALTER TABLE sala
 ADD CONSTRAINT todosSonJugadores
-CHECK (ALL (listaJugadores) in usuario.Nombre);
+CHECK (ALL (listaJugadores) in usuario.Nombre); */
+
 
 --partida constraints
 ALTER TABLE partida
-ADD CONSTRAINT pk_codigo
+ADD CONSTRAINT part_pk_codigo
 PRIMARY KEY (codigo);
 
 ALTER TABLE partida
-ADD CONSTRAINT fk_snombre
+ADD CONSTRAINT part_fk_snombre
 FOREIGN KEY (ServidorNombre)
 REFERENCES servidor(Nombre);
 
 ALTER TABLE partida
-ADD CONSTRAINT fk_scodigo
+ADD CONSTRAINT part_fk_scodigo
 FOREIGN KEY (SalaCodigo)
 REFERENCES sala(codigo);
 
@@ -49,38 +53,41 @@ CHECK (EscenarioNombre IN ( SELECT nombre
                             WHERE tipo="Escenario")
 );
 
+
 --juega constraints
 ALTER TABLE juega
-ADD CONSTRAINT pk_usuario-partida
+ADD CONSTRAINT juega_pk_usuario_partida
 PRIMARY KEY (UsuarioNombre, PartidaCodigo);
 
 ALTER TABLE juega
-ADD CONSTRAINT fk_usuario
+ADD CONSTRAINT juega_fk_usuario
 FOREIGN KEY (UsuarioNombre)
 REFERENCES usuario(nombre);
 
 ALTER TABLE juega
-ADD CONSTRAINT fk_partida
+ADD CONSTRAINT juega_fk_partida
 FOREIGN KEY (PartidaCodigo)
 REFERENCES partida(codigo);
 
+
 --item constraints
 ALTER TABLE item
-ADD CONSTRAINT pk_nombre
+ADD CONSTRAINT item_pk_nombre
 PRIMARY KEY (Nombre);
+
 
 --posee constraints
 ALTER TABLE posee
-ADD CONSTRAINT pk_usuario-item
+ADD CONSTRAINT posee_pk_usuario_item
 PRIMARY KEY (UsuarioNombre, ItemNombre);
 
 ALTER TABLE posee
-ADD CONSTRAINT fk_usuario
+ADD CONSTRAINT posee_fk_usuario
 FOREIGN KEY (UsuarioNombre)
 REFERENCES usuario(nombre);
 
 ALTER TABLE posee
-ADD CONSTRAINT fk_item
+ADD CONSTRAINT posee_fk_item
 FOREIGN KEY (ItemNombre)
 REFERENCES item(Nombre);
 
