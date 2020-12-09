d807965439d502bc252a159a2c200b7a62610162
Sebastian Knell <sebastianknell@hotmail.com>
2020-12-08 21:52:50 -0500

    more random functions

--- connector.py
+++ connector.py
@@ -1,19 +1,21 @@
-import psycopg2, generator as gen, string
+import psycopg2, generator as gen
 
-DATABASE = "bd1"
-SCHEMA = "dbproject"
+DATABASE = "db1project"
+SCHEMA = "db_project100"
 # Query templates
 INSERT = "insert into {} values ({})"
-
-# Sample dictionaries
-usuario_tipos = ['j', 'c', 's']
+SELECT = "select {} from {} where {}"
 
 def main():
+    # Connect to database
     conn = psycopg2.connect(database=DATABASE, options="-c search_path={}".format(SCHEMA))
     curr = conn.cursor()
+    n = 10
 
-    insertUsuario(curr, 10)
+    # insertUsuario(curr, n)
+    insertServidor(curr)
     
+    # Commit and close session
     conn.commit()
     curr.close()
     conn.close()
@@ -29,8 +31,21 @@ def insertUsuario(curr, n):
     table = "usuario(nombre, tipo)"
     values = valuesFormat(2)
     for x in range(n):
-        query = INSERT.format(table, values.format(gen.rand_str(string.ascii_letters, 30), gen.rand_str(usuario_tipos, 1)))
+        query = INSERT.format(table, values.format(gen.get_usuario(), gen.get_usuario_tipo()))
         curr.execute(query)
+
+def insertServidor(curr):
+    table = "servidor(nombre, usuarionombre, ip, activo, jugadores, flujo, idioma, locacion)"
+    values = valuesFormat(8)
+    # TODO get usuarios that are also hosts
+    curr.execute(SELECT.format('*', 'usuario', "tipo='s'"))
+    queryResult = curr.fetchall()
+    for row in queryResult:
+        usuario = row[0]
+        for i in range(gen.rand_num(5)):
+            curr.execute(INSERT.format(table, values.format(gen.get_nombre(),
+            usuario, gen.get_ip(), gen.rand_bool(), gen.rand_num(10), gen.rand_num(10),
+            gen.get_idioma(), gen.get_locacion())))
 
 
 main()
--- generator.py
+++ generator.py
@@ -1,7 +1,35 @@
-import random as rd
+import random as rd, string
+
+# Sample dictionaries
+usuario_tipos = ['j', 'c', 's']
+numbers = ['1','2','3','4','5','6','7','8','9','.']
+idiomas = ['Spanish', 'English', 'German', 'French', 'Portuguese', 'Chinese']
+paises = ['Spain', 'US', 'Germany', 'France', 'Portugal', 'China']
+bools = [True, False]
 
 def rand_num(max):
     return rd.randint(0, max)
 
 def rand_str(dict, size):
     return ''.join(rd.choice(dict) for x in range(size))
+
+def rand_bool():
+    return rd.choice(bools)
+
+def get_usuario():
+    return rand_str(string.ascii_letters, 30)
+
+def get_usuario_tipo():
+    return rand_str(usuario_tipos, 1)
+
+def get_ip():
+    return rand_str(numbers, 15)
+
+def get_locacion():
+    return rand_str(paises, 1)
+
+def get_idioma():
+    return rand_str(idiomas, 1)
+
+def get_nombre():
+    return rand_str(string.ascii_letters, 35)
