b46e7b4065b9677fac58c3df3aded84d06ded640
Sebastian Knell <sebastianknell@hotmail.com>
2020-12-09 11:20:17 -0500

    connector updated

--- connector.py
+++ connector.py
@@ -1,7 +1,7 @@
 import psycopg2, generator as gen
 
 DATABASE = "db1project"
-SCHEMA = "db_project100"
+SCHEMA = "db_project10_3"
 # Query templates
 INSERT = "insert into {} values ({})"
 SELECT = "select {} from {} where {}"
@@ -28,24 +28,24 @@ def valuesFormat(n):
     return values
 
 def insertUsuario(curr, n):
-    table = "usuario(nombre, tipo)"
-    values = valuesFormat(2)
+    table = "usuario(nombre, is_jugador, is_comerciante, is_server_owner)"
+    values = valuesFormat(4)
     for x in range(n):
-        query = INSERT.format(table, values.format(gen.get_usuario(), gen.get_usuario_tipo()))
+        query = INSERT.format(table, values.format(gen.get_usuario(), gen.rand_bool(),
+        gen.rand_bool(), gen.rand_bool()))
         curr.execute(query)
 
 def insertServidor(curr):
-    table = "servidor(nombre, usuarionombre, ip, activo, jugadores, flujo, idioma, locacion)"
-    values = valuesFormat(8)
-    curr.execute(SELECT.format('nombre', 'usuario', "tipo='s'"))
+    table = "servidor(nombre, usuario_nombre, ip, activo, idioma, locacion)"
+    values = valuesFormat(6)
+    curr.execute(SELECT.format('nombre', 'usuario', "is_server_owner='true'"))
     queryResult = curr.fetchall()
     # Add 0 to 5 servidores for each user that is a server-owner
     for row in queryResult:
         usuario = row[0]
         for i in range(gen.rand_num(5)):
             curr.execute(INSERT.format(table, values.format(gen.get_nombre(),
-            usuario, gen.get_ip(), gen.rand_bool(), gen.rand_num(10), gen.rand_num(10),
-            gen.get_idioma(), gen.get_locacion())))
+            usuario, gen.get_ip(), gen.rand_bool(), gen.get_idioma(), gen.get_locacion())))
 
 def insertSala(curr):
     table = "sala(servidornombre, codigo, enjuego)"
