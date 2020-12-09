0b8808a837438d1f4f99e8b93e4e95c06c377606
Sebastian Knell <sebastianknell@hotmail.com>
2020-12-09 11:39:49 -0500

    generic insert function

--- connector.py
+++ connector.py
@@ -1,19 +1,27 @@
 import psycopg2, generator as gen
 
 DATABASE = "db1project"
-SCHEMA = "db_project10_3"
+SCHEMA1 = "db_project10_3"
+SCHEMA2 = "db_project10_4"
+SCHEMA3 = "db_project10_5"
+SCHEMA4 = "db_project10_6"
 # Query templates
 INSERT = "insert into {} values ({})"
 SELECT = "select {} from {} where {}"
 
 def main():
-    # Connect to database
-    conn = psycopg2.connect(database=DATABASE, options="-c search_path={}".format(SCHEMA))
-    curr = conn.cursor()
-    n = 10
+    insertData(1000, SCHEMA1)
+    # insertData(10000, SCHEMA2)
+    # insertData(100000, SCHEMA3)
+    # insertData(1000000, SCHEMA4)
 
-    # insertUsuario(curr, n)
-    insertServidor(curr)
+def insertData(n , schema):
+    # Connect to database
+    conn = psycopg2.connect(database=DATABASE, options="-c search_path={}".format(schema))
+    curr = conn.cursor()
+    # Insert data
+    insertUsuario(curr, n)
+    insertServidor(curr, n)
     
     # Commit and close session
     conn.commit()
@@ -35,7 +43,7 @@ def insertUsuario(curr, n):
         gen.rand_bool(), gen.rand_bool()))
         curr.execute(query)
 
-def insertServidor(curr):
+def insertServidor(curr, n):
     table = "servidor(nombre, usuario_nombre, ip, activo, idioma, locacion)"
     values = valuesFormat(6)
     curr.execute(SELECT.format('nombre', 'usuario', "is_server_owner='true'"))
@@ -43,12 +51,12 @@ def insertServidor(curr):
     # Add 0 to 5 servidores for each user that is a server-owner
     for row in queryResult:
         usuario = row[0]
-        for i in range(gen.rand_num(5)):
+        for i in range(gen.rand_num(n / 100)):
             curr.execute(INSERT.format(table, values.format(gen.get_nombre(),
             usuario, gen.get_ip(), gen.rand_bool(), gen.get_idioma(), gen.get_locacion())))
 
 def insertSala(curr):
-    table = "sala(servidornombre, codigo, enjuego)"
+    table = "sala(servidor_nombre, codigo, enjuego)"
     values = valuesFormat(3)
     curr.execute(SELECT.format('nombre', 'servidor'))
     queryResult = curr.fetchall()
